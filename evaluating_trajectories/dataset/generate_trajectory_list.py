fec51e72e95071d0fd393ece3e5dc11f188414f7
tkietreiber <145535134+tkietreiber@users.noreply.github.com>
2024-08-20 14:40:19 +0200

    feat: use pure duckdb in generate_trajectory_list.py
    Co-authored-by: Alessio Molinari <alessio.molinari@strg.at>

--- evaluating_trajectories/dataset/generate_trajectory_list.py
+++ evaluating_trajectories/dataset/generate_trajectory_list.py
@@ -5,13 +5,8 @@ import duckdb
 dataset_path = "2019-Oct.csv"
 output_file = "trajectories.pkl"
 
-df = duckdb.sql(f"SELECT * FROM '{dataset_path}'").df()
-events_list = (
-    df.sort_values(by=["user_session", "event_time"])
-    .groupby("user_session", sort=False)
-    .apply(lambda x: x.to_dict(orient="records"))
-    .tolist()
-)  # needs a LOT of memory!
+columns = duckdb.sql(f"SELECT * FROM '{dataset_path}' LIMIT 1").columns
+data = duckdb.sql(f"SELECT list(struct_pack({','.join(columns)})) FROM '{dataset_path}' GROUP BY user_session")
 
 
 with open(output_file, "wb") as f:
