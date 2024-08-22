0d1c59deb2dfdcd6d93aaf70712a0b637d91f844
tkietreiber <145535134+tkietreiber@users.noreply.github.com>
2024-08-22 10:51:27 +0200

    Update evaluating_trajectories/dataset/generate_trajectory_list.py
    Co-authored-by: Alessio Molinari <alessio.molinari@strg.at>

--- evaluating_trajectories/dataset/generate_trajectory_list.py
+++ evaluating_trajectories/dataset/generate_trajectory_list.py
@@ -6,10 +6,6 @@ dataset_path = "2019-Oct.csv"
 output_file = "trajectories.parquet"
 
 columns = duckdb.sql(f"SELECT * FROM '{dataset_path}' LIMIT 1").columns
-data = duckdb.sql(
-    f"SELECT list(struct_pack({','.join(columns)})) FROM '{dataset_path}' GROUP BY user_session"
-)
-
 duckdb.sql(
     f"COPY (SELECT list(struct_pack({','.join(columns)})) FROM '{dataset_path}' GROUP BY user_session) TO '{output_file}'"
 )
